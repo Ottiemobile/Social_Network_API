1c7004507654b953ae60c5fc67a7a459c8ed24ee
Ethan Torres <torresethan99@gmail.com>
2020-10-21 17:46:16 -0500

    need help with middleware

--- server.js
+++ server.js
@@ -5,8 +5,11 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
+
 app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
 app.use(routes);
 
 db.once('open', () => {
